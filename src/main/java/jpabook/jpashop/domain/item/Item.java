5791157625028817306e2c6985a4607c0a40edd6
sirzzang <sirzzang@naver.com>
2022-07-15 17:59:56 +0900

    temporary

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,26 @@ public abstract class Item {
     // 양방향 n:n
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        
+        // 유효성 검증
+        if (restStock < 0) {
+            throw new NotEnoughStockException("Needs more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
